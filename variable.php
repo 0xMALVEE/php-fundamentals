693771d79f97806746e1c4b984172d76261ced52
Alvee <35369563+0xMALVEE@users.noreply.github.com>
2022-07-25 10:51:35 +0600

    Create variable.php

---
+++ variable.php
@@ -0,0 +1,27 @@
+<?php 
+
+// Data Types -> String, Int, Float, Bool, Array, Objects, Null, more
+
+// String
+$name = "Alvee";
+
+// Float
+$float = 13.3;
+
+// Bool
+$bool = false;
+
+// Array
+$arr = array(234,"Alvee","88",55);
+
+
+$age = 23;
+
+// Outputs 
+$sen = $name . " is " . $age . " years old!";
+echo $sen;
+print_r($arr[0]) ;
+
+
+
+
