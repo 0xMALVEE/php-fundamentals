f965a46a1221ada9070fe22e2a0dac663bf6818a
Alvee <35369563+0xMALVEE@users.noreply.github.com>
2022-07-25 14:45:45 +0600

    +

---
+++ contant_and_operators.php
@@ -0,0 +1,39 @@
+<?php
+
+// Constants and Operators
+
+// $var = "23";
+
+// $var = "44";
+
+// echo $var;
+
+define("str",34);
+
+// echo str;
+
+// Arithmetic operators (+, -. *, /, **, %)
+// Comparison operators (==, ===, !=, !==, >, <, >=, <=)
+// Increment / Decrement Operators (++$x, $x++, --$x, $x--)
+// Logical Operators (and / &&, or / || , !)
+
+$x = 11;
+$y = 2;
+
+// echo $x * $y;
+// 2^4 (2*2*2*2)
+// echo 2 ** 4;
+
+// echo $x % $y;
+
+// echo 2 === "2";
+// true = 1, false = nothing
+// echo 4 > 2;
+// echo 2 > 1;
+// echo 4 >= 4;
+
+// echo $x++;
+
+// echo true and true; ( needs 2 true)
+// echo true or false; (needs min 1 true)
+// echo !false; (changes to oposite bool)
---
+++ math_functions.php
@@ -0,0 +1,17 @@
+<?php
+
+// Math Functions - pi() , min() , max(), abs(), sqrt() , rand() 
+
+// echo pi();
+
+// echo min(1,2,3,4,5,100,100,-200, 445,45);
+
+// echo max(1,2,3,4,5,100,100,-200, 445,45);
+
+// echo abs( -65 * 3 );
+
+// echo sqrt(16);
+// echo " ";
+// echo sqrt(64);
+
+// echo rand(1,1000);
