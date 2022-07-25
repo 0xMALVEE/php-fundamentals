58d5da4bd0dacb7517164cdd69d910587667dc7f
Alvee <35369563+0xMALVEE@users.noreply.github.com>
2022-07-25 11:42:57 +0600

    Create string_functions.php

---
+++ string_functions.php
@@ -0,0 +1,23 @@
+<?php
+
+// Common PHP String Functions!
+// strlen() , str_word_count() , strrev() , strpos() , str_replace() 
+
+$str = "php php dog cat";
+
+
+// strlen
+strlen( $str );
+
+// str_word_count
+str_word_count($str);
+
+// strrev
+strrev($str);
+
+// strpos
+strpos($str, "cat" );
+
+// str_replace
+str_replace("cat", "dog", $str);
+
