47dd1df2a459d4717ba7eda111d8aaf864fbb34b
Alvee <35369563+0xMALVEE@users.noreply.github.com>
2022-08-05 00:20:13 +0600

    +

--- function.php
+++ function.php
@@ -1,9 +1,35 @@
 <?php
 
 // practice question 0 : write a function that prints "hello world"!
+function printing_stuff(){
+    echo "hello world!";
+}
+
+// printing_stuff();
 
 // practice question 1: write a function that takes in 2 numbers and prints the sum of them
+function add($number1, $number2){
+    echo $number1 + $number2;
+}
 
+// add(3,6);
 // practice question 2: write a function that takes in 1 number and prints all the numbers starting for 1 till that number
+function looping($x){
+    for($i = 1; $i <= $x;$i++){
+        echo $i;
+    }
+    echo "\n";
+}
+// looping(4);
+// looping(100);
 
 // practice question 3: write a function that takes 2 number and checks if they are equal if they are then return true or else return false.
+function check_numbers($num1 , $num2){
+    if($num1 == $num2){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+echo check_numbers(1,2);
--- loops.php
+++ loops.php
@@ -1,14 +1,33 @@
 <?php
 
 // practice question 0: print numbers from 100 to 200 using a for loop
+// for($i = 100;$i <= 200;$i++){
+//     echo $i;
+//     echo " ";
+// }
 
 $sentence = "I love birds!";
 // practice question 1: print each of the character in the $sentence variable (Using a for loop)
-
+for($i = 0;$i < strlen($sentence); $i++){
+    // echo $sentence[$i]  . " ";
+}
 
 $counter = 1;
 // practice question 2: print all the numbers from 1 to 20 using a while loop
+while($counter <= 20){
+    // echo $counter . " ";
+    $counter++;
+}
 
-
-$arr = array("Dog", "Cat", "Bird", 55,66);
+$arr = array("Dog", "Cat", "Bird", 55,66, true);
 // practice question 3: print all the elements of this array (using a regular for loop and using a foreach loop)
+
+for($i = count($arr);$i >= 0; $i--){
+    // echo $arr[$i];
+    // echo " ";
+}
+
+foreach($arr as $index => $values){
+    // echo $values;echo " ";
+    echo $index;
+}
